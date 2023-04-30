762a0d0d0fd5a7e70b3338261b54dcba44d28850
cornelius-student <2023506@student.uwtsd.ac.uk>
2023-04-30 16:18:36 +0100

    Updating Version 1 - Correcting incorrect push
    Setting version 1 back to the default from incorrect push

--- Network programmability - Assignment v 1.0 .py
+++ Network programmability - Assignment v 1.0 .py
@@ -1,16 +1,14 @@
 import requests
 
-
 # Set up the API endpoint URL and parameters
-url = "http://api.openweathermap.org/data/2.5/weather?"
+url = "http://api.openweathermap.org/data/2.5/weather"
 params = {
-    "lat": "44.36",
-    "lon": "39.64,"
+    "q": "London,UK",
     "appid": "f4a703089e2e872b0a5c010b2c9e3a31",
     "units": "metric"
 }
 # Send a request to the API
-response = requests.get (f'http://api.openweathermap.org/data/2.5/weather?"{q}&appid={appid}')
+response = requests.get(url, params=params)
 
 # If the response was successful, parse the JSON data and print the current temperature
 if response.status_code == 200:
