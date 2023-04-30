20f12b0cdb60f383ad0175b5a702366e9aa1f8d8
1809784 <1809784@student.uwtsd.ac.uk>
2023-04-30 16:12:48 +0100

    Feedback - v1.0 Jonney
    Just had a look at the v1.1 file, not sure if the issue is mainly based on the fact it needs the latitude and longitude rather than city name?

--- Network programmability - Assignment v 1.0 .py
+++ Network programmability - Assignment v 1.0 .py
@@ -2,14 +2,15 @@ import requests
 
 
 # Set up the API endpoint URL and parameters
-url = "http://api.openweathermap.org/data/2.5/weather"
+url = "http://api.openweathermap.org/data/2.5/weather?"
 params = {
-    "q": "London,UK",
+    "lat": "44.36",
+    "lon": "39.64,"
     "appid": "f4a703089e2e872b0a5c010b2c9e3a31",
     "units": "metric"
 }
 # Send a request to the API
-response = requests.get(url, params=params)
+response = requests.get (f'http://api.openweathermap.org/data/2.5/weather?"{q}&appid={appid}')
 
 # If the response was successful, parse the JSON data and print the current temperature
 if response.status_code == 200:
